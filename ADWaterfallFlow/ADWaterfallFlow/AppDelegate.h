007f4c0a115b83983986e64698487aaf28375c9c
zhongaidong <1042480866@qq.com>
2016-06-23 14:33:37 +0800

    初始化项目

---
+++ ADWaterfallFlow/ADWaterfallFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADWaterfallFlow
+//
+//  Created by zhongaidong on 16/6/23.
+//  Copyright © 2016年 zhongaidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADWaterfallFlow/ADWaterfallFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADWaterfallFlow
+//
+//  Created by zhongaidong on 16/6/23.
+//  Copyright © 2016年 zhongaidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
